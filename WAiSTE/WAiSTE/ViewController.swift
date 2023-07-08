be7b7ccee56ad7e3dafb6b0253f8a8047504dd0d
Airei Fukuzawa <t-afukuzawa@microsoft.com>
2023-07-07 20:53:18 -0700

    First commit

---
+++ WAiSTE/WAiSTE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WAiSTE
+//
+//  Created by Airei Fukuzawa on 7/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
